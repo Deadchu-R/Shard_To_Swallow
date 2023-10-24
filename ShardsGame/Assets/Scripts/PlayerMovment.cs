f7e7d33d06fe0c885620381c9ec50c9eca409eef
RoxasCodes <evicokazzz@gmail.com>
2023-10-24 16:28:56 +0300

    first push camera and player

---
+++ ShardsGame/Assets/Scripts/PlayerMovment.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsometricCharacterMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 inputDirection = new Vector3(horizontalInput, 0, verticalInput).normalized;
+
+        if (inputDirection != Vector3.zero)
+        {
+            Vector3 moveDirection = Camera.main.transform.TransformDirection(inputDirection);
+            moveDirection.y = 0;
+
+            transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+        }
+    }
+}
