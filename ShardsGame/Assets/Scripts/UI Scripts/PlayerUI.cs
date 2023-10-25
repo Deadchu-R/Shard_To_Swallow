503beec8613d4ecfa9f58122dccd1b2dd4a16b63
Roee Tal <roeetal17@gmail.com>
2023-10-25 18:37:09 +0300

    Question  Text
    Added Question Text object
    and a Question Text in PlayerUI Scripts

--- ShardsGame/Assets/Scripts/UI Scripts/PlayerUI.cs
+++ ShardsGame/Assets/Scripts/UI Scripts/PlayerUI.cs
@@ -9,12 +9,15 @@ using UnityEngine.Events;
 public class PlayerUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _healthText;
-
+    [SerializeField] private TextMeshProUGUI _questionText;
     private void Awake()
     {
         _healthText.text = "0";
     }
-
+    public void SetQuestionText(string text)
+    {
+        _questionText.text = text;
+    }
     public void UpdateHealthText(int health)
     {
         _healthText.text = health.ToString();
