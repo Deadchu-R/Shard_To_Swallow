94f7e3bfe6eee3c18459a8b9ec254dc0cd1a12bb
Roee Tal <roeetal17@gmail.com>
2023-10-25 22:16:23 +0300

    MainMenuSceen + script

---
+++ ShardsGame/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+   [SerializeField] private GameObject mainMenuPanel;
+   [SerializeField] private GameObject creditsPanel;
+   [SerializeField] private GameObject optionsPanel;
+
+    public void StartLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+    
+    public void OpenCredits()
+    {
+        mainMenuPanel.SetActive(false);
+        creditsPanel.SetActive(true);
+    }
+    
+    public void OpenOptions()
+    {
+        mainMenuPanel.SetActive(false);
+        optionsPanel.SetActive(true);
+    }
+}
